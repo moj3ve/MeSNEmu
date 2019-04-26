093659867759997d0c7bab3c2933d8c3e582ebd2
SarahH12099 <sarahh12099@gmail.com>
2019-04-26 14:25:13 -0400

    Initial commit

--- Snes9x/sa1.cpp
+++ Snes9x/sa1.cpp
@@ -1,9 +1,3 @@
-/*****************************************************************************\
- Snes9x - Portable Super Nintendo Entertainment System (TM) emulator.
- This file is licensed under the Snes9x License.
- For further information, consult the LICENSE file in the root directory.
- \*****************************************************************************/
-
 #include "snes9x.h"
 #include "memmap.h"
 
