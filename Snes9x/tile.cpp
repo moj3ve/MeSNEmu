58c8def90fdc78a6a18ff9636eb2b3dd259914a9
SarahH12099 <sarahh12099@gmail.com>
2019-05-24 12:38:24 -0400

    Initial commit

--- Snes9x/tile.cpp
+++ Snes9x/tile.cpp
@@ -9,7 +9,6 @@ static uint32	pixbit[8][16];
 static uint8	hrbit_odd[256];
 static uint8	hrbit_even[256];
 
-
 void S9xInitTileRenderer (void)
 {
 	int	i;
@@ -514,7 +513,8 @@ void S9xSelectTileConverter (int depth, bool8 hires, bool8 sub, bool8 mosaic)
 	}
 
 #define IS_BLANK_TILE() \
-	(BG.Buffered[TileNumber] == BLANK_TILE)
+    ( ( (Tile & H_FLIP) ? BG.BufferedFlip[TileNumber] : BG.Buffered[TileNumber]) == BLANK_TILE)
+	// (BG.Buffered[TileNumber] == BLANK_TILE)
 
 #define SELECT_PALETTE() \
 	if (BG.DirectColourMode) \
@@ -568,14 +568,9 @@ void S9xSelectTileConverter (int depth, bool8 hires, bool8 sub, bool8 mosaic)
 		bp = pCache + BPSTART; \
 		for (l = LineCount; l > 0; l--, bp += 8 * PITCH, Offset += GFX.PPL) \
 		{ \
-			DRAW_PIXEL(0, Pix = bp[0]); \
-			DRAW_PIXEL(1, Pix = bp[1]); \
-			DRAW_PIXEL(2, Pix = bp[2]); \
-			DRAW_PIXEL(3, Pix = bp[3]); \
-			DRAW_PIXEL(4, Pix = bp[4]); \
-			DRAW_PIXEL(5, Pix = bp[5]); \
-			DRAW_PIXEL(6, Pix = bp[6]); \
-			DRAW_PIXEL(7, Pix = bp[7]); \
+            for (int x = 0; x < 8; x++) { \
+                DRAW_PIXEL(x, Pix = bp[x]); \
+            } \
 		} \
 	} \
 	else \
@@ -584,14 +579,9 @@ void S9xSelectTileConverter (int depth, bool8 hires, bool8 sub, bool8 mosaic)
 		bp = pCache + BPSTART; \
 		for (l = LineCount; l > 0; l--, bp += 8 * PITCH, Offset += GFX.PPL) \
 		{ \
-			DRAW_PIXEL(0, Pix = bp[7]); \
-			DRAW_PIXEL(1, Pix = bp[6]); \
-			DRAW_PIXEL(2, Pix = bp[5]); \
-			DRAW_PIXEL(3, Pix = bp[4]); \
-			DRAW_PIXEL(4, Pix = bp[3]); \
-			DRAW_PIXEL(5, Pix = bp[2]); \
-			DRAW_PIXEL(6, Pix = bp[1]); \
-			DRAW_PIXEL(7, Pix = bp[0]); \
+            for (int x = 0; x < 8; x++) { \
+                DRAW_PIXEL(x, Pix = bp[7 - x]); \
+            } \
 		} \
 	} \
 	else \
@@ -600,14 +590,9 @@ void S9xSelectTileConverter (int depth, bool8 hires, bool8 sub, bool8 mosaic)
 		bp = pCache + 56 - BPSTART; \
 		for (l = LineCount; l > 0; l--, bp -= 8 * PITCH, Offset += GFX.PPL) \
 		{ \
-			DRAW_PIXEL(0, Pix = bp[0]); \
-			DRAW_PIXEL(1, Pix = bp[1]); \
-			DRAW_PIXEL(2, Pix = bp[2]); \
-			DRAW_PIXEL(3, Pix = bp[3]); \
-			DRAW_PIXEL(4, Pix = bp[4]); \
-			DRAW_PIXEL(5, Pix = bp[5]); \
-			DRAW_PIXEL(6, Pix = bp[6]); \
-			DRAW_PIXEL(7, Pix = bp[7]); \
+            for (int x = 0; x < 8; x++) { \
+                DRAW_PIXEL(x, Pix = bp[x]); \
+            } \
 		} \
 	} \
 	else \
@@ -615,14 +600,9 @@ void S9xSelectTileConverter (int depth, bool8 hires, bool8 sub, bool8 mosaic)
 		bp = pCache + 56 - BPSTART; \
 		for (l = LineCount; l > 0; l--, bp -= 8 * PITCH, Offset += GFX.PPL) \
 		{ \
-			DRAW_PIXEL(0, Pix = bp[7]); \
-			DRAW_PIXEL(1, Pix = bp[6]); \
-			DRAW_PIXEL(2, Pix = bp[5]); \
-			DRAW_PIXEL(3, Pix = bp[4]); \
-			DRAW_PIXEL(4, Pix = bp[3]); \
-			DRAW_PIXEL(5, Pix = bp[2]); \
-			DRAW_PIXEL(6, Pix = bp[1]); \
-			DRAW_PIXEL(7, Pix = bp[0]); \
+            for (int x = 0; x < 8; x++) { \
+                DRAW_PIXEL(x, Pix = bp[7 - x]); \
+            } \
 		} \
 	}
 
