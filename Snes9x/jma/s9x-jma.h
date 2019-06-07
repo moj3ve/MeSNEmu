6660075c8b21c0ec6e8523cfe5d9303a01103256
SarahH12099 <sarahh12099@gmail.com>
2019-06-06 20:44:44 -0400

    Initial commit

--- Snes9x/jma/s9x-jma.h
+++ Snes9x/jma/s9x-jma.h
@@ -1,3 +1,13 @@
+/*****************************************************************************\
+     Snes9x - Portable Super Nintendo Entertainment System (TM) emulator.
+                This file is licensed under the Snes9x License.
+   For further information, consult the LICENSE file in the root directory.
+\*****************************************************************************/
+
+//  JMA compressed file support
+//  (c) Copyright 2004-2006 NSRT Team (http://nsrt.edgeemu.com)
+
+
 #ifdef __cplusplus
 extern "C" {
 #endif
