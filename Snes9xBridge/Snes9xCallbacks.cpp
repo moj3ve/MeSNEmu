89b54e40f5f94cfd769d7370d65f11b57a745c0a
SarahH12099 <sarahh12099@gmail.com>
2019-06-29 13:24:08 -0400

    Initial commit

--- Snes9xBridge/Snes9xCallbacks.cpp
+++ Snes9xBridge/Snes9xCallbacks.cpp
@@ -1,5 +1,9 @@
 #include "Snes9xCallbacks.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/time.h>
 #include <libgen.h>
 
@@ -9,6 +13,8 @@
 #include "../Snes9x/conffile.h"
 #include "../Snes9x/controls.h"
 #include "../Snes9x/display.h"
+
+#define MAX_MESSAGE_LEN (36 * 3)
 
 #include "Snes9xMain.h"
 #include "iOSAudio.h"
@@ -32,363 +38,346 @@ int SI_SleptLastFrame = 0;
 
 #pragma mark - SNES9X Callbacks
 
-void S9xExit ()
+void S9xExit()
 {
 }
 
 void S9xParsePortConfig(ConfigFile &a, int pass)
 {
-  
+    
 }
 
 void S9xExtraUsage (void)
 {
-  
+    
 }
 
 void S9xParseArg (char** a, int &b, int c)
 {
-  
+    
 }
 
-const char* S9xGetDirectory (enum s9x_getdirtype dirtype)
+void S9xMessage(int /*type */, int /*number */, const char *message)
 {
-  static int	index = 0;
-  static char	path[4][PATH_MAX + 1];
-  
-  char	inExt[16];
-  char	drive[_MAX_DRIVE + 1], dir[_MAX_DIR + 1], fname[_MAX_FNAME + 1], ext[_MAX_EXT + 1];
-  
-  index++;
-  if (index > 3)
-    index = 0;
-  
-  switch (dirtype)
-  {
-    case SNAPSHOT_DIR:		strcpy(inExt, ".frz");	break;
-    case SRAM_DIR:			strcpy(inExt, ".srm");	break;
-    case SCREENSHOT_DIR:	strcpy(inExt, ".png");	break;
-    case SPC_DIR:			strcpy(inExt, ".spc");	break;
-    case CHEAT_DIR:			strcpy(inExt, ".cht");	break;
-    case BIOS_DIR:			strcpy(inExt, ".bio");	break;
-    case LOG_DIR:			strcpy(inExt, ".log");	break;
-    default:				strcpy(inExt, ".xxx");	break;
-  }
-  
-  _splitpath(S9xGetFilename(inExt, dirtype), drive, dir, fname, ext);
-  _makepath(path[index], drive, dir, "", "");
-  
-  int	l = strlen(path[index]);
-  if (l > 1)
-    path[index][l - 1] = 0;
-  
-  return (path[index]);
+    printf("%s", message);
 }
 
-bool8 S9xDoScreenshot (int width, int height)
+void S9xAutoSaveSRAM()
 {
-  return true;
+    SISaveSRAM();
 }
 
-const char* S9xStringInput (const char* s)
+void S9xToggleSoundChannel(int c)
 {
-  return (NULL);
+    static int sound_switch = 255;
+    
+    if (c == 8)
+        sound_switch = 255;
+    else
+        sound_switch ^= 1 << c;
+    
+    S9xSetSoundControl (sound_switch);
 }
 
-void S9xHandlePortCommand (s9xcommand_t cmd, int16 data1, int16 data2)
+bool8 S9xOpenSoundDevice(void)
 {
-  return;
+    if(SI_SoundOn)
+        return TRUE;
+    else
+        return FALSE;
 }
 
-bool S9xPollAxis (uint32 id, int16* value)
+/* eke-eke */
+void S9xInitSync()
 {
-  return (false);
 }
 
-void S9xToggleSoundChannel (int c)
-{
-  static int	channel_enable = 255;
-  
-  if (c == 8)
-    channel_enable = 255;
-  else
-    channel_enable ^= 1 << c;
-  
-  S9xSetSoundControl(channel_enable);
+/*** Synchronisation ***/
+
+void S9xSyncSpeed() {
+    struct timeval now;
+    
+    // calculate lag
+    gettimeofday (&now, NULL);
+    
+    if (SI_NextFrameTime.tv_sec == 0)
+    {
+        SI_NextFrameTime = now;
+        ++SI_NextFrameTime.tv_usec;
+    }
+    int lag = TIMER_DIFF (now, SI_NextFrameTime);
+    SI_FrameTimeDebt += lag-(int)Settings.FrameTime;
+    //printf("Frame Time: %i. Should be less than %i\n", lag, (int)Settings.FrameTime);
+    
+    // if we're  going too fast
+    bool sleptThis = 0;
+    if(SI_FrameTimeDebt < 0 && IPPU.SkippedFrames == 0)
+        //if(debt+(int)Settings.FrameTime < 0 && IPPU.SkippedFrames == 0)
+    {
+        int audioOffset = SIAudioOffset();
+        if(-SI_FrameTimeDebt+audioOffset > 0)
+            usleep(-SI_FrameTimeDebt+audioOffset);
+        //usleep(-(debt+(int)Settings.FrameTime));
+        SI_FrameTimeDebt = 0;
+        sleptThis = 1;
+    }
+    
+    // if we're going too slow or fixed frameskip
+    if (Settings.SkipFrames == AUTO_FRAMERATE && !Settings.SoundSync)
+    {
+        // auto frameskip
+        if(SI_FrameTimeDebt > (int)Settings.FrameTime*10 || IPPU.SkippedFrames >= 2)
+            SI_FrameTimeDebt = 0;
+        
+        if(SI_FrameTimeDebt > 0 && SI_SleptLastFrame == 0)
+        {
+            IPPU.RenderThisFrame = 0;
+            IPPU.SkippedFrames++;
+        }
+        else
+        {
+            IPPU.RenderThisFrame = 1;
+            IPPU.SkippedFrames = 0;
+        }
+    }
+    else
+    {
+        // frameskip a set number of frames
+        if(IPPU.SkippedFrames < Settings.SkipFrames)
+        {
+            IPPU.RenderThisFrame = 0;
+            IPPU.SkippedFrames++;
+        }
+        else
+        {
+            IPPU.RenderThisFrame = 1;
+            IPPU.SkippedFrames = 0;
+        }
+    }
+    
+    if(sleptThis == 1)
+        SI_SleptLastFrame = 1;
+    else
+        SI_SleptLastFrame = 0;
+    
+    //next_frame_time = now;
+    gettimeofday (&SI_NextFrameTime, NULL);
 }
 
-bool S9xPollButton (uint32 id, bool* pressed)
+bool8 S9xInitUpdate()
 {
-  *pressed = false;
-  return true;
+    return (TRUE);
 }
 
-bool8 S9xContinueUpdate (int width, int height)
+bool8 S9xDoScreenshot (int Width, int Height)
 {
-  return (true);
-}
-
-bool S9xPollPointer (uint32 id, int16* x, int16* y)
-{
-  *x = *y = 0;
-  
-  return (true);
-}
-
-const char* S9xChooseFilename (bool8 read_only)
-{
-  return (NULL);
-}
-
-const char* S9xChooseMovieFilename (bool8 read_only)
-{
-  return (NULL);
-}
-
-void S9xSetPalette ()
-{
-  
+    return true;
 }
 
 bool8 S9xOpenSnapshotFile (const char* fname, bool8 read_only, STREAM* file)
 {
-  if (read_only)
-  {
-		if (0 != (*file = OPEN_STREAM(fname, "rb")))
-      return (true);
-  }
-  else
-  {
-		if (0 != (*file = OPEN_STREAM(fname, "wb")))
-      return (true);
-  }
-  
-  return (false);
+    if (read_only)
+    {
+        if (0 != (*file = OPEN_STREAM(fname, "rb")))
+            return (true);
+    }
+    else
+    {
+        if (0 != (*file = OPEN_STREAM(fname, "wb")))
+            return (true);
+    }
+    
+    return (false);
 }
 
 void S9xCloseSnapshotFile (STREAM file)
 {
-  CLOSE_STREAM(file);
+    CLOSE_STREAM(file);
 }
 
-void S9xMessage (int /* type */, int /* number */, const char* message)
+bool8 S9xDeinitUpdate(int Width, int Height)
 {
-  printf ("%s\n", message);
+    SIFlipFramebuffer(Width, Height);
+    return (TRUE);
 }
 
-bool8_32 S9xInitUpdate ()
+bool8 S9xContinueUpdate(int Width, int Height)
 {
-  //GFX.Screen = (uint8*) framebuffer16 + (640*8) + 64;
-  
-  return (TRUE);
+    return (TRUE);
 }
 
-bool8 S9xDeinitUpdate (int width, int height)
+void S9xSetPalette()
 {
-  // TODO clear Z buffer if not in fastsprite mode
-  SIFlipFramebuffer(width, height);
-  
-  return (TRUE);
+    return;
 }
 
-const char* S9xGetFilename (const char* ex, enum s9x_getdirtype dirtype)
+void S9xHandlePortCommand(s9xcommand_t cmd, int16 data1, int16 data2)
 {
-  static char filename [PATH_MAX + 1];
-  char drive [_MAX_DRIVE + 1];
-  char dir [_MAX_DIR + 1];
-  char fname [_MAX_FNAME + 1];
-  char ext [_MAX_EXT + 1];
-  
-  _splitpath (Memory.ROMFilename, drive, dir, fname, ext);
-  //strcpy (filename, SIGetSnapshotDirectory());
-  strcpy (filename, "");
-  strcat (filename, SLASH_STR);
-  strcat (filename, fname);
-  strcat (filename, ex);
-  return (filename);
+    return;
 }
 
-//const char* S9xGetFilenameInc (const char* e)
-const char* S9xGetFilenameInc (const char* inExt, enum s9x_getdirtype dirtype)
+bool S9xPollButton(uint32 id, bool * pressed)
 {
-  S9xMessage (0,0,"get filename inc");
-  //return e;
-  return inExt;
+    return 0;
 }
 
-void S9xSyncSpeed(void)
+bool S9xPollAxis(uint32 id, int16 * value)
 {
-  struct timeval now;
-  
-  // calculate lag
-  gettimeofday (&now, NULL);
-  
-  if (SI_NextFrameTime.tv_sec == 0)
-  {
-    SI_NextFrameTime = now;
-    ++SI_NextFrameTime.tv_usec;
-  }
-  int lag = TIMER_DIFF (now, SI_NextFrameTime);
-  SI_FrameTimeDebt += lag-(int)Settings.FrameTime;
-  //printf("Frame Time: %i. Should be less than %i\n", lag, (int)Settings.FrameTime);
-  
-  // if we're  going too fast
-  bool sleptThis = 0;
-  if(SI_FrameTimeDebt < 0 && IPPU.SkippedFrames == 0)
-  //if(debt+(int)Settings.FrameTime < 0 && IPPU.SkippedFrames == 0)
-  {
-    int audioOffset = SIAudioOffset();
-    if(-SI_FrameTimeDebt+audioOffset > 0)
-      usleep(-SI_FrameTimeDebt+audioOffset);
-    //usleep(-(debt+(int)Settings.FrameTime));
-    SI_FrameTimeDebt = 0;
-    sleptThis = 1;
-  }
-  
-  // if we're going too slow or fixed frameskip
-  if (Settings.SkipFrames == AUTO_FRAMERATE && !Settings.SoundSync)
-  {
-    // auto frameskip
-    if(SI_FrameTimeDebt > (int)Settings.FrameTime*10 || IPPU.SkippedFrames >= 2)
-      SI_FrameTimeDebt = 0;
+    return 0;
+}
+
+bool S9xPollPointer(uint32 id, int16 * x, int16 * y)
+{
+    return 0;
+}
+
+const char *S9xChooseFilename(bool8 read_only)
+{
+    return NULL;
+}
+
+const char * S9xChooseMovieFilename(bool8 read_only)
+{
+    return NULL;
+}
+
+const char * S9xGetDirectory(enum s9x_getdirtype dirtype)
+{
+    static int index = 0;
+    static char path[4][PATH_MAX + 1];
     
-    if(SI_FrameTimeDebt > 0 && SI_SleptLastFrame == 0)
+    char    inExt[16];
+    char    drive[_MAX_DRIVE + 1], dir[_MAX_DIR + 1], fname[_MAX_FNAME + 1], ext[_MAX_EXT + 1];
+    
+    index++;
+    if (index > 3)
+        index = 0;
+    
+    switch (dirtype)
     {
-      IPPU.RenderThisFrame = 0;
-      IPPU.SkippedFrames++;
+        case SNAPSHOT_DIR:        strcpy(inExt, ".frz");    break;
+        case SRAM_DIR:            strcpy(inExt, ".srm");    break;
+        case SCREENSHOT_DIR:    strcpy(inExt, ".png");    break;
+        case SPC_DIR:            strcpy(inExt, ".spc");    break;
+        case CHEAT_DIR:            strcpy(inExt, ".cht");    break;
+        case BIOS_DIR:            strcpy(inExt, ".bio");    break;
+        case LOG_DIR:            strcpy(inExt, ".log");    break;
+        default:                strcpy(inExt, ".xxx");    break;
+    }
+    
+    _splitpath(S9xGetFilename(inExt, dirtype), drive, dir, fname, ext);
+    _makepath(path[index], drive, dir, "", "");
+    
+    int    l = strlen(path[index]);
+    if (l > 1)
+        path[index][l - 1] = 0;
+    
+    return (path[index]);
+}
+
+const char * S9xGetFilename(const char *ex, enum s9x_getdirtype dirtype)
+{
+    static char filename [PATH_MAX + 1];
+    char drive [_MAX_DRIVE + 1];
+    char dir [_MAX_DIR + 1];
+    char fname [_MAX_FNAME + 1];
+    char ext [_MAX_EXT + 1];
+    
+    _splitpath (Memory.ROMFilename, drive, dir, fname, ext);
+    strcpy (filename, "");
+    strcat (filename, SLASH_STR);
+    strcat (filename, fname);
+    strcat (filename, ex);
+    return (filename);
+}
+
+const char * S9xGetFilenameInc(const char *e, enum s9x_getdirtype dirtype)
+{
+    S9xMessage (0,0,"get filename inc");
+    return e;
+}
+
+const char * S9xBasename(const char *name)
+{
+    static char    s[PATH_MAX + 1];
+    
+    strncpy(s, name, PATH_MAX + 1);
+    s[PATH_MAX] = 0;
+    
+    size_t    l = strlen(s);
+    
+    for (unsigned int i = 0; i < l; i++)
+    {
+        if (s[i] < 32 || s[i] >= 127)
+            s[i] = '_';
+    }
+    
+    return (basename(s));
+}
+
+const char * S9xStringInput (const char * s)
+{
+    return s;
+}
+
+void _splitpath(char const *buf, char *drive, char *dir, char *fname, char *ext)
+{
+    *drive = 0;
+    
+    char const * slash = strrchr (buf, '/');
+    if (!slash)
+        slash = strrchr (buf, '\\');
+    
+    char const * dot = strrchr (buf, '.');
+    
+    if (dot && slash && dot < slash)
+        dot = NULL;
+    
+    if (!slash)
+    {
+        strcpy (dir, "");
+        strcpy (fname, buf);
+        if (dot)
+        {
+            *(fname + (dot - buf)) = 0;
+            strcpy (ext, dot + 1);
+        }
+        else
+            strcpy (ext, "");
     }
     else
     {
-      IPPU.RenderThisFrame = 1;
-      IPPU.SkippedFrames = 0;
+        strcpy (dir, buf);
+        *(dir + (slash - buf)) = 0;
+        strcpy (fname, slash + 1);
+        if (dot)
+        {
+            *(fname + (dot - slash) - 1) = 0;
+            strcpy (ext, dot + 1);
+        }
+        else
+            strcpy (ext, "");
     }
-  }
-  else
-  {
-    // frameskip a set number of frames
-    if(IPPU.SkippedFrames < Settings.SkipFrames)
+}
+
+void _makepath(char *filename, const char *drive, const char *dir, const char *fname, const char *ext)
+{
+    if (dir && *dir)
     {
-      IPPU.RenderThisFrame = 0;
-      IPPU.SkippedFrames++;
+        strcpy (filename, dir);
+        strcat (filename, "/");
     }
     else
+        *filename = 0;
+    strcat (filename, fname);
+    if (ext && *ext)
     {
-      IPPU.RenderThisFrame = 1;
-      IPPU.SkippedFrames = 0;
+        strcat (filename, ".");
+        strcat (filename, ext);
     }
-  }
-  
-  if(sleptThis == 1)
-    SI_SleptLastFrame = 1;
-  else
-    SI_SleptLastFrame = 0;
-  
-  //next_frame_time = now;
-  gettimeofday (&SI_NextFrameTime, NULL);
 }
 
-const char* S9xBasename (const char* in)
+int access(const char *pathname, int mode)
 {
-  /*const char* p;
-  
-  S9xMessage (0,0,"s9x base name");
-  
-  if ((p = strrchr (f, '/')) != NULL || (p = strrchr (f, '\\')) != NULL)
-    return (p + 1);
-  
-  return (f);*/
-  
-  static char	s[PATH_MAX + 1];
-  
-	strncpy(s, in, PATH_MAX + 1);
-	s[PATH_MAX] = 0;
-  
-	size_t	l = strlen(s);
-  
-	for (unsigned int i = 0; i < l; i++)
-  {
-		if (s[i] < 32 || s[i] >= 127)
-			s[i] = '_';
-	}
-  
-	return (basename(s));
-}
-
-//};
-
-bool8 S9xOpenSoundDevice (void)
-{
-  if(SI_SoundOn)
-    return TRUE;
-  else
-    return FALSE;
-}
-
-void S9xAutoSaveSRAM (void)
-{
-	//since I can't sync the data, there is no point in even writing the data
-	//out at this point.  Instead I'm now saving the data as the users enter the menu.
-  SISaveSRAM();
-	//sync();  can't sync when emulator is running as it causes delays
-}
-
-#pragma mark - OS-Related Path Manipulation
-
-void _makepath (char* path, const char*, const char* dir,
-                const char* fname, const char* ext)
-{
-	if (dir && *dir)
-	{
-		strcpy (path, dir);
-		strcat (path, "/");
-	}
-	else
-    *path = 0;
-	strcat (path, fname);
-	if (ext && *ext)
-	{
-		strcat (path, ".");
-		strcat (path, ext);
-	}
-}
-
-void _splitpath (const char* path, char* drive, char* dir, char* fname,
-                 char* ext)
-{
-	*drive = 0;
-  
-	char const * slash = strrchr (path, '/');
-	if (!slash)
-		slash = strrchr (path, '\\');
-  
-	char const * dot = strrchr (path, '.');
-  
-	if (dot && slash && dot < slash)
-		dot = NULL;
-  
-	if (!slash)
-	{
-		strcpy (dir, "");
-		strcpy (fname, path);
-		if (dot)
-		{
-			*(fname + (dot - path)) = 0;
-			strcpy (ext, dot + 1);
-		}
-		else
-			strcpy (ext, "");
-	}
-	else
-	{
-		strcpy (dir, path);
-		*(dir + (slash - path)) = 0;
-		strcpy (fname, slash + 1);
-		if (dot)
-		{
-			*(fname + (dot - slash) - 1) = 0;
-			strcpy (ext, dot + 1);
-		}
-		else
-			strcpy (ext, "");
-	}
+    return 1;
 }
